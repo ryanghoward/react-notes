aa9f1034546e6b05a7d2540853ed231ef884f3dd
Ryan Howard <ryanghoward@protonmail.com>
2023-01-24 14:28:05 -0800

    React-icons

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { CiSearch } from "react-icons/ci";
 
 const Notes = () => {
-  return <div>Notes</div>;
+  return (
+    <section>
+      <header className='notes__header'>
+        <h2>My Notes</h2>
+        {/* <input type='text' autoFocus placeholder='Create a Note' /> */}
+        <button className='btn'>
+          <CiSearch />
+        </button>
+      </header>
+    </section>
+  );
 };
 
 export default Notes;
