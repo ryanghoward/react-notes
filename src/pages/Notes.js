903688e1236ea25edd0e1fd05d656746100a40c9
Ryan Howard <ryanghoward@protonmail.com>
2023-01-24 14:41:16 -0800

    save

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,5 +1,9 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import { CiSearch } from "react-icons/ci";
+import { BsPlusLg } from "react-icons/bs";
+import dummyNotes from "../dummy_notes";
+
+import NoteItem from "../components/NoteItem";
 
 const Notes = () => {
   return (
@@ -11,6 +15,14 @@ const Notes = () => {
           <CiSearch />
         </button>
       </header>
+      <div className='notes__container'>
+        {dummyNotes.map((note) => (
+          <NoteItem key={note.id} note={note} />
+        ))}
+      </div>
+      <Link className='btn add__btn'>
+        <BsPlusLg />
+      </Link>
     </section>
   );
 };
